75f48f114150abe6aa4644679b9e9eb3102377e4
Your Name <you@example.com>
2020-11-24 18:10:55 +0900

    fd

--- trdg/string_generator.py
+++ trdg/string_generator.py
@@ -114,3 +114,42 @@ def create_strings_randomly(length, allow_variable, count, let, num, sym, lang):
             current_string += " "
         strings.append(current_string[:-1])
     return strings
+
+def create_strings_randomly2(length, allow_variable, count, let, num, sym, lang):
+    """
+        Create all strings by randomly sampling from a pool of characters.
+    """
+
+    # If none specified, use all three
+    if True not in (let, num, sym):
+        let, num, sym = True, True, True
+
+    pool = ""
+    if let:
+        if lang == "cn":
+            pool += "".join(
+                [chr(i) for i in range(19968, 40908)]
+            )  # Unicode range of CHK characters
+        else:
+            pool += string.ascii_letters
+    if num:
+        pool += "0123456789"
+    if sym:
+        pool += "!\"#$%&'()*+,-./:;?@[\\]^_`{|}~"
+
+    if lang == "cn":
+        min_seq_len = 1
+        max_seq_len = 2
+    else:
+        min_seq_len = 2
+        max_seq_len = 10
+
+    strings = []
+    for _ in range(0, count):
+        current_string = ""
+        for _ in range(0, rnd.randint(1, length) if allow_variable else length):
+            seq_len = rnd.randint(min_seq_len, max_seq_len)
+            current_string += "".join([rnd.choice(pool) for _ in range(seq_len)])
+            current_string += " "
+        strings.append(current_string[:-1])
+    return strings
